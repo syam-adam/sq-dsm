9190085be6cf68697467eaea33a29ba2129fb0fe
Neal H. Walfield <neal@sequoia-pgp.org>
2026-02-07 12:00:32 +0100

    openpgp: Run leak tests on all backends.

--- openpgp/tests/secret-leak-detector.rs
+++ openpgp/tests/secret-leak-detector.rs
@@ -207,8 +207,6 @@ mod leak_tests {
     make_test!(test_encrypted);
     make_test!(test_password);
     make_test!(test_ed25519);
-    #[cfg(not(feature = "crypto-rust"))]
     make_test!(test_aes_256_encryption);
-    #[cfg(not(feature = "crypto-rust"))]
     make_test!(test_aes_256_decryption);
 }
